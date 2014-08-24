9e48c4ca222f6b95516750e7aabcc1a8da6e9a68
kokakolako <niklas.koehler@live.de>
2014-08-24 14:15:00 +0200

    Renaming: zsh/alias.sh -> alias.sh
    I moved the alias.sh file to the global config-files because the
    alias.sh file should be not exclusive binded to zsh.

--- alias.sh
+++ alias.sh
@@ -3,7 +3,7 @@
 
 alias iconfig="vim $HOME/.config/i3/config +'lcd $HOME/.config/i3'"
 alias vconfig="vim $HOME/.config/vim/vimrc $HOME/.config/vim/extend.vim $HOME/.config/vim/plugins.vim +'lcd $HOME/.config/vim'"
-alias zconfig="vim $HOME/.config/zsh/zshrc $HOME/.config/zsh/alias.sh +'lcd $HOME/.config/zsh'"
+alias zconfig="vim $HOME/.config/zsh/zshrc $HOME/.config/alias.sh +'lcd $HOME/.config/zsh'"
 alias uconfig="vim $HOME/.config/X11/Xresources +'lcd $HOME/.config/X11'"
 alias nconfig="vim $HOME/.config/ncmpcpp/config +'lcd $HOME/.config/ncmpcpp'"
 alias sconfig="vim $HOME/.config/sxhkd/sxhkdrc +'lcd $HOME/.config/sxhkd'"
@@ -11,12 +11,14 @@ alias hconfig="vim $HOME/.config/herbstluftwm/autostart $HOME/.config/herbstluft
 alias tconfig="vim $HOME/.config/tmux/tmux.conf +'lcd $HOME/.config/tmux'"
 alias rconfig="sudo vim /etc/minirc.conf /sbin/rc"
 
+
 # Start program with a customized config-file
 # ---------------------------------------------
 
 alias ncmpcpp="ncmpcpp -c $HOME/.config/ncmpcpp/config"
 alias pandoc="pandoc --latex-engine=xelatex"
 alias mpv="mpv -input file=$HOME/.config/mpv/mpv-control"
+
 
 # Shortcuts (+ sudo-shortcuts)
 # ---------------------------------------------
@@ -32,6 +34,7 @@ alias shutdown="sudo shutdown"
 alias reboot="sudo reboot"
 alias poweroff="sudo poweroff"
 
+
 # Misc aliases
 # ---------------------------------------------
 
@@ -41,30 +44,43 @@ alias uzbl="uzbl-tabbed"       # Starting uzbl as uzbl-tabbed
 alias info="info --vi-keys"    # Info with vi-keybindings
 alias grep="grep --color=auto" # Colorize output of grep
 alias cp="rsync -aP"           # Show a progress bar and do not ignore permissions
+alias ipv4_addr="curl ipv4.icanhazip.com"
+alias ipv6_addr="curl ipv6.icanhazip.com"
+
+
+# Hook which is invoked when the current working directory is changing
+# ---------------------------------------------
+
+function chpwd () {
+    ## If the current working directory contains lesser than 30 directories,
+    ## show the directories of the current working directory via ls
+    [[ $( ls -l | wc -l ) -lt 65 ]] && ls
+}
+
 
 # Remove all orphans
 # ---------------------------------------------
 
 remove_orphans () {
     echo "\e[34m::\e[0;1m Entferne nicht-benötigte Pakete...\e[0m"
-    pacman -Qdt
+    pacaur -Qdt # List all orphans
     if [[ $? == 0 ]]; then
         local orphans=$( pacaur -Qdt | wc -l )
-        if [[ $orphans == 1 ]]; then
-            sudo pacman -Rns $( pacaur -Qdt | sed -E 's/\s(([0-9]*[\.\-])*)[0-9]*//g' )
-        elif [[ $orphans > 1 ]]; then
-            sudo pacman -Rns $( pacaur -Qdt | sed -E 's/\s(([0-9]*[\.\-])*)[0-9]*//g' | tr "\n" " " )
-        fi
-        [[ $? == 0 ]] && echo " Es wurden alle nicht-benötigte Pakete entfernt"
+        [[ $orphans -ge 1 ]] \
+            && sudo pacaur -Rns $( pacaur -Qdtq )
+        [[ $? == 0 ]] \
+            && echo " Es wurden alle nicht-benötigte Pakete entfernt" \
+            || echo " Beim Entfernen der nicht-benötigten Pakete kam es zu einem Fehler"
     else
         echo " Es gibt nichts zu tun"
     fi
 }
 
+
 # Update git submodules
 # ---------------------------------------------
 
-update_git_submodules () {
+function update_git_submodules () {
     local curr_dir=$PWD
     echo "\e[34m::\e[0;1m Aktualisieren der git submodules...\e[0m"
     submodules=("$HOME/.config/vim/")
@@ -75,71 +91,79 @@ update_git_submodules () {
     cd "$curr_dir"
 }
 
+
 # Update the complete system
 # ---------------------------------------------
 
-upd () {
-    pacaur -Syu
-    pacaur -Sc
-    remove_orphans
-    update_git_submodules
+function upd () {
+    pacaur -Syu             ## Update all packages
+    pacaur -Sc              ## Clear the cache
+    remove_orphans          ## Remove all orphans
+    update_git_submodules   ## Update all submodules
 }
+
 
 # Colorizing Man-Pages
 # ---------------------------------------------
 
-man () {
-    env LESS_TERMCAP_mb=$'\E[01;31m' \
-    LESS_TERMCAP_md=$'\E[01;38;5;74m' \
-    LESS_TERMCAP_me=$'\E[0m' \
-    LESS_TERMCAP_se=$'\E[0m' \
-    LESS_TERMCAP_so=$'\E[38;5;246m' \
-    LESS_TERMCAP_ue=$'\E[0m' \
-    LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+function man () {
+    env LESS_TERMCAP_mb=$'\e[36m'   \
+    LESS_TERMCAP_md=$'\e[36m'       \
+    LESS_TERMCAP_me=$'\e[0m'        \
+    LESS_TERMCAP_se=$'\e[0m'        \
+    LESS_TERMCAP_so=$'\e[1m'        \
+    LESS_TERMCAP_ue=$'\e[0m'        \
+    LESS_TERMCAP_us=$'\e[31m'       \
     man "$@"
 }
+
 
 # Update my own, local packages
 # ---------------------------------------------
 
-update_my_own_pkgs () {
+function update_my_own_pkgs () {
     for i in "$( find -type d $HOME/.abs )"; do
         cd $i
     done
 }
 
+
 # Reload tmux
 # ---------------------------------------------
 
-tmux-reload () {
+function tmux-reload () {
     tmux source-file $HOME/.config/tmux/tmux.conf > /dev/null
     [ $? -eq 0 ] \
         && echo "Sucessfully reloaded the tmux.conf file" \
         || echo "An error occured while trying to reload the tmux.conf file"
 }
 
+
 # Reload sxhkd
 # ---------------------------------------------
 
-sxhkd-reload () {
+function sxhkd-reload () {
     kill -s USR1 "$( pidof sxhkd )" > /dev/null
     [ $? -eq 0 ] \
         && echo "Sucessfully reloaded the sxhkdrc file" \
         || echo "An error occured while trying to reload the sxhkdrc file"
 }
 
-# Colorized "dirs -v"
+
+# Colorized "dirs", "pushd", "popd"
 # ---------------------------------------------
 
-stack () {
-    lines=$( dirs -v | wc -l )
-    lines=$(( $lines - 1 ))
+function dirs () {
+    ## To use the builtin dirs command the command must be invoked via "builtin"
+    local lines=$( builtin dirs -v | wc -l )
+    local lines=$(( $lines - 1 ))
     for i in $( seq 0 $lines ); do
+        local args="$@"
         local stack_pos="$i"
         local color=
-        ## Do not remove the "-w" parameter of grep, beacause with "-w", grep
+        ## Do not remove the "-w" parameter of grep, because with "-w" grep
         ## is only searching the first matching
-        local dir=$( dirs -v | grep -w "$stack_pos" | cut -c 3- )
+        local dir=$( builtin dirs -v | grep -w "$stack_pos" | cut -c 3- )
         ## Check if $i is lesser or equal to 5, to use them as a color-code
         if [[ $i -le 5 ]]; then
             color=$(( $i + 1 ))
@@ -151,10 +175,27 @@ stack () {
             ## is totally boring
             color=$(( $color + 1 ))
         fi
-        echo "${stack_pos}: \e[3${color}m${dir}\e[0m"
+        ## When "pushd" is invoked, the pushed directory should be hightlighted
+        case $args in
+            "$HOME" )
+                [[ "$dir" == "~" ]] \
+                    && echo "\e[1m${stack_pos}\t\e[3${color}m${dir}\e[0m" \
+                    || echo "${stack_pos}\t\e[3${color}m${dir}\e[0m"
+            ;;
+            "$dir" )
+                echo "\e[1m${stack_pos}\t\e[3${color}m${dir}\e[0m"
+            ;;
+            * )
+                echo "${stack_pos}\t\e[3${color}m${dir}\e[0m"
+            ;;
+        esac
     done
 }
 
-alias popd="popd; stack"
-alias dirs="dirs -v"
+function pushd () {
+    local args="$@"
+    builtin pushd "$args" > /dev/null
+    dirs "$args"
+}
 
+alias popd="popd "$@" > /dev/null; dirs"
